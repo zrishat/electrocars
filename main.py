152f65f571911b0b946e4e70a344895f86cca758
Rishat Zainullin <r.zainullin@omp.ru>
2021-12-02 00:44:41 +0300

    Flask with admin-panel and templates

--- main.py
+++ main.py
@@ -1,284 +1,229 @@
-from sqlalchemy import (
-    create_engine,
-    Column,
-    Integer,
-    String,
-    Boolean,
-    DateTime,
-    ForeignKey,
-    func, Table,
-)
-from sqlalchemy.orm import declarative_base, sessionmaker, relationship, joinedload
+import os
+from flask import Flask, url_for, redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from wtforms import form, fields, validators
+import flask_admin as admin
+import flask_login as login
+from flask_admin.contrib import sqla
+from flask_admin import helpers, expose
+from werkzeug.security import generate_password_hash, check_password_hash
 
-engine = create_engine('sqlite:///database.db', echo=True)
-Session = sessionmaker(bind=engine)
+# Create Flask application
+app = Flask(__name__)
 
-Base = declarative_base(bind=engine)
+# Create dummy secrey key so we can use sessions
+app.config['SECRET_KEY'] = '123456790'
 
+# Create in-memory database
+app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+app.config['DATABASE_FILE'] = 'sample_db.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + app.config['DATABASE_FILE']
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+# Create user model.
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
+    login = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120))
+    password = db.Column(db.String(64))
+
+    # Flask-Login integration
+    # NOTE: is_authenticated, is_active, and is_anonymous
+    # are methods in Flask-Login < 0.3.0
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.id
+
+    # Required for administrative interface
+    def __unicode__(self):
+        return self.username
+
+
+# Define login and registration forms (for flask-login)
+class LoginForm(form.Form):
+    login = fields.StringField('Email Address', [validators.DataRequired(message='Field required')])
+    password = fields.PasswordField('Password', [validators.DataRequired(message='Field required')])
+
+    def validate_login(self, field):
+        user = self.get_user()
+
+        if user is None:
+            raise validators.ValidationError('Invalid user')
+
+        # we're comparing the plaintext pw with the the hash from the db
+        if not check_password_hash(user.password, self.password.data):
+        # to compare plain text passwords use
+        # if user.password != self.password.data:
+            raise validators.ValidationError('Invalid password')
+
+    def get_user(self):
+        return db.session.query(User).filter_by(login=self.login.data).first()
+
+
+class RegistrationForm(form.Form):
+    login = fields.StringField('Email Address', [validators.DataRequired(message='Field required')])
+    password = fields.PasswordField('Password', [validators.DataRequired(message='Field required')])
+
+    # login = fields.StringField(validators=[validators.required()])
+    email = fields.StringField()
+    # password = fields.PasswordField(validators=[validators.required()])
 
-class CreatedAtMixin:
-    created_at = Column(DateTime, nullable=False, server_default=func.now())
+    def validate_login(self, field):
+        if db.session.query(User).filter_by(login=self.login.data).count() > 0:
+            raise validators.ValidationError('Duplicate username')
 
 
-class User(CreatedAtMixin, Base):
-    __tablename__ = "users"
+# Initialize flask-login
+def init_login():
+    login_manager = login.LoginManager()
+    login_manager.init_app(app)
 
-    id = Column(Integer, primary_key=True)
-    username = Column(String, nullable=False, unique=True)
-    is_staff = Column(Boolean, nullable=False, default=False)
+    # Create user loader function
+    @login_manager.user_loader
+    def load_user(user_id):
+        return db.session.query(User).get(user_id)
 
-    posts = relationship("Post", back_populates="author")
 
-    def __str__(self):
-        return f"{self.__class__.__name__}(id={self.id}," \
-               f" username={self.username!r}," \
-               f" is_staff={self.is_staff}," \
-               f" created_at={self.created_at})"
+# Create customized model view class
+class MyModelView(sqla.ModelView):
 
-    def __repr__(self):
-        return str(self)
+    def is_accessible(self):
+        return login.current_user.is_authenticated
 
 
-posts_tags_association_table = Table(
-    "posts_tags_association_table",
-    Base.metadata,
-    Column("post_id", ForeignKey("posts.id"), primary_key=True),
-    Column("tag_id", ForeignKey("tags.id"), primary_key=True),
-)
+# Create customized index view class that handles login & registration
+class MyAdminIndexView(admin.AdminIndexView):
 
-
-class Post(CreatedAtMixin, Base):
-    __tablename__ = "posts"
-
-    id = Column(Integer, primary_key=True)
-    title = Column(String, nullable=False, server_default="")
-
-    author_id = Column(ForeignKey(User.id), nullable=False, unique=False)
-    author = relationship("User", back_populates="posts")
-
-    tags = relationship(
-        "Tag",
-        back_populates="posts",
-        secondary=posts_tags_association_table,
-    )
-
-    def __str__(self):
-        return f"{self.__class__.__name__}(id={self.id}," \
-               f" title={self.title!r}" \
-               f" created_at={self.created_at})"
-
-    def __repr__(self):
-        return str(self)
-
-
-class Tag(Base):
-    __tablename__ = "tags"
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False, unique=True)
-
-    posts = relationship(
-        "Post",
-        back_populates="tags",
-        secondary=posts_tags_association_table,
-    )
-
-    def __str__(self):
-        return self.name
-
-    def __repr__(self):
-        return str(self)
-
-
-def create_admin_user():
-    session = Session()
-
-    user_admin = User(username="admin", is_staff=True)
-    print(user_admin)
-
-    session.add(user_admin)
-    session.commit()
-    print(user_admin)
-
-    session.close()
-
-
-def create_users():
-    session = Session()
-
-    user_john = User(username="john")
-    user_sam = User(username="sam")
-    print(user_john)
-    print(user_sam)
-
-    session.add(user_john)
-    session.add(user_sam)
-    session.commit()
-
-    print(user_john)
-    print(user_sam)
-
-    session.close()
-
-
-def get_all_users():
-    session = Session()
-
-    users = session.query(User).all()
-
-    print("users:", users)
-
-    session.close()
-
-
-def get_user_by_username():
-    session = Session()
-    session.query(User).filter_by(username="sam").one_or_none()
-    session.close()
-
-
-def get_users_by_username():
-    session = Session()
-
-    users = session.query(User).filter(
-        User.username.in_(["sam", "john"])).all()
-    print(users)
-
-    session.close()
-
-
-def create_posts_for_users():
-    session = Session()
-    user_sam = session.query(User).filter_by(username="sam").one()
-    user_john = session.query(User).filter_by(username="john").one()
-
-    post_1 = Post(title="Tesla Model X", author=user_sam)
-    post_2 = Post(title="Nissan Leaf", author=user_john)
-
-    session.add(post_1)
-    session.add(post_2)
-    session.commit()
-
-    session.close()
-
-
-def get_all_posts_with_authors():
-    session = Session()
-
-    posts = session.query(Post).options(joinedload(Post.author)).all()
-
-    for post in posts:  # type: Post
-        print("post", post)
-        print("post author", post.author)
-
-    session.close()
-
-
-def get_all_users_with_posts():
-    session = Session()
-
-    users = session.query(User).options(joinedload(User.posts)).all()
-
-    for user in users:  # type: User
-        print("user", user)
-        print("user's posts", user.posts)
-
-    session.close()
-
-
-def create_tags():
-    session = Session()
-
-    tags_names = ["news", "tesla", "audi", "nissan"]
-
-    tags = [Tag(name=name) for name in tags_names]
-
-    for tag in tags:
-        session.add(tag)
-
-    session.commit()
-
-    for tag in tags:
-        print(tag.id, tag)
-
-    session.close()
-
-
-def assign_tags():
-    session = Session()
-
-    tags = session.query(Tag).all()
-    tags_map = {tag.name: tag for tag in tags}
-    tag_tesla = tags_map.pop("tesla")
-
-    posts = session.query(Post).options(joinedload(Post.tags)).all()
-
-    for post in posts:  # type: Post
-        print("post", post, "with tags", post.tags)
-        for tag_name, tag in tags_map.items():
-            if tag_name in post.title.lower():
-                post.tags.append(tag)
-                print("assigned tag", tag, "to post", post)
-        post.tags.append(tag_tesla)
-
-    session.commit()
-
-    posts = session.query(Post).options(joinedload(Post.tags)).all()
-
-    for post in posts:  # type: Post
-        print("post", post, "with tags", post.tags)
-
-    session.close()
-
-
-def show_posts_with_tags_and_authors():
-    session = Session()
-
-    posts = (
-        session.query(Post)
-        .options(
-            joinedload(Post.tags)
-        )
-        .options(
-            joinedload(Post.author)
-        )
-        .all()
-    )
-
-    for post in posts:  # type: Post
-        print("++post", post)
-        print("with tags", post.tags)
-        print("and author", post.author)
-
-    session.close()
-
-
-def find_users_by_tags():
-    session = Session()
-
-    users = (
-        session.query(User)
-        .join(User.posts)
-        .join(Post.tags)
-        .filter(Tag.name == "news")
-        .all()
-    )
-
-    for user in users:
-        print("user", user)
-
-    session.close()
-
-
-def main():
-    Base.metadata.create_all()
-    create_admin_user()
-    create_users()
-    create_posts_for_users()
-    create_tags()
-    assign_tags()
-    show_posts_with_tags_and_authors()
+    @expose('/')
+    def index(self):
+        if not login.current_user.is_authenticated:
+            return redirect(url_for('.login_view'))
+        return super(MyAdminIndexView, self).index()
+
+    @expose('/login/', methods=('GET', 'POST'))
+    def login_view(self):
+        # handle user login
+        form = LoginForm(request.form)
+        if helpers.validate_form_on_submit(form):
+            user = form.get_user()
+            login.login_user(user)
+
+        if login.current_user.is_authenticated:
+            return redirect(url_for('.index'))
+        link = '<p>Don\'t have an account? <a href="' + url_for('.register_view') + '">Click here to register.</a></p>'
+        self._template_args['form'] = form
+        self._template_args['link'] = link
+        return super(MyAdminIndexView, self).index()
+
+    @expose('/register/', methods=('GET', 'POST'))
+    def register_view(self):
+        form = RegistrationForm(request.form)
+        if helpers.validate_form_on_submit(form):
+            user = User()
+
+            form.populate_obj(user)
+            # we hash the users password to avoid saving it as plaintext in the db,
+            # remove to use plain text:
+            user.password = generate_password_hash(form.password.data)
+
+            db.session.add(user)
+            db.session.commit()
+
+            login.login_user(user)
+            return redirect(url_for('.index'))
+        link = '<p>Already have an account? <a href="' + url_for('.login_view') + '">Click here to log in.</a></p>'
+        self._template_args['form'] = form
+        self._template_args['link'] = link
+        return super(MyAdminIndexView, self).index()
+
+    @expose('/logout/')
+    def logout_view(self):
+        login.logout_user()
+        return redirect(url_for('.index'))
+
+@app.route("/")
+def homepage():
+    return render_template('homepage.html', title_name='Homepage')
+
+
+@app.route("/page.html")
+def page():
+    return render_template('page.html', title_name='Page')
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html', title_name='404'), 404
+
+
+# Initialize flask-login
+init_login()
+
+# Create admin
+admin = admin.Admin(app, 'Electrocars: Auth', index_view=MyAdminIndexView(), base_template='my_master.html', template_mode='bootstrap4')
+
+# Add view
+admin.add_view(MyModelView(User, db.session))
+
+
+def build_sample_db():
+    """
+    Populate a small db with some example entries.
+    """
+
+    import string
+    import random
+
+    db.drop_all()
+    db.create_all()
+    # passwords are hashed, to use plaintext passwords instead:
+    # test_user = User(login="test", password="test")
+    test_user = User(login="test", password=generate_password_hash("test"))
+    db.session.add(test_user)
+
+    first_names = [
+        'Harry', 'Amelia', 'Oliver', 'Jack', 'Isabella', 'Charlie','Sophie', 'Mia'
+    ]
+    last_names = [
+        'Brown', 'Smith', 'Patel', 'Jones', 'Williams', 'Johnson', 'Taylor', 'Thomas',
+    ]
+
+    for i in range(len(first_names)):
+        user = User()
+        user.first_name = first_names[i]
+        user.last_name = last_names[i]
+        user.login = user.first_name.lower()
+        user.email = user.login + "@example.com"
+        user.password = generate_password_hash(''.join(random.choice(string.ascii_lowercase + string.digits) for i in range(10)))
+        db.session.add(user)
+
+    db.session.commit()
+    return
 
 
 if __name__ == '__main__':
-    main()
+
+    # Build a sample db on the fly, if one does not exist yet.
+    app_dir = os.path.realpath(os.path.dirname(__file__))
+    database_path = os.path.join(app_dir, app.config['DATABASE_FILE'])
+    if not os.path.exists(database_path):
+        build_sample_db()
+
+    # Start app
+    app.run(host='0.0.0.0', port=5000, debug=False, threaded=False)
--- readme.md
+++ readme.md
@@ -1,3 +1,16 @@
-Run main.py for creating database.db. 
+For build
+```
+docker image build -t electrocars:latest .
+```
+or use compose file
+```
+docker-compose up --build
+```
 
-After you can run tests for check records in database.
+From console open example url http://172.19.0.2:5000/ with web browser.
+
+You can open:
+- homepage http://172.19.0.2:5000/
+- page http://172.19.0.2:5000/page.html
+- 404 page example http://172.19.0.2:5000/page2.html
+- admin panel http://172.19.0.2:5000/admin/
